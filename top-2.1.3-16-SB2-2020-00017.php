0a4f8e3b2d9af45c69f2142c700e3bcf084f7170
Peter Vekony <vekony@lovelysystems.com>
2025-01-21 16:44:19 +0100

    fix upload dates

--- top-2.1.3-16-SB2-2020-00017.php
+++ top-2.1.3-16-SB2-2020-00017.php
@@ -29,9 +29,9 @@ include('header.php');
                     <p>
                         Projekt azonosítója: <strong>TOP-2.1.3-16-SB2-2020-00017</strong><br />
                         <br />
-                        Sajtóközlemény a projekt elindulásáról: <a href="docs/top-2.1.3-16-SB2-2020-00017_sajto_nyito.pdf" target="_blank">letöltés</a> -  feltöltve: 2025. 01. 20.
+                        Sajtóközlemény a projekt elindulásáról: <a href="docs/top-2.1.3-16-SB2-2020-00017_sajto_nyito.pdf" target="_blank">letöltés</a> -  feltöltve: 2021.06.30
                         <br />
-                        Sajtóközlemény a projekt zárásáról: <a href="docs/top-2.1.3-16-SB2-2020-00017_sajto_zaro.pdf" target="_blank">letöltés</a> -  feltöltve: 2025. 01. 20.
+                        Sajtóközlemény a projekt zárásáról: <a href="docs/top-2.1.3-16-SB2-2020-00017_sajto_zaro.pdf" target="_blank">letöltés</a> -  feltöltve: 2023.11.30
                         <br />
                     </p>
                 </div>
