5883962cfbb4ae8e269799ff28788851537f693e
vekonypeter <vekonypeter94@gmail.com>
2025-02-07 17:43:59 +0100

    remove visible footer

--- footer.php
+++ footer.php
@@ -1,7 +1,3 @@
-<div id="footer">
-    <span style="color: aquamarine">Készítette:</span><span style="color: white"> Vékony Péter -- <span style="color: aquamarine">E-mail:</span> <a href="mailto:vekonypeter@vikingcomp.hu">vekonypeter@vikingcomp.hu</a></span>
-</div>
-
 </div>
 
 <!-- jQuery -->
