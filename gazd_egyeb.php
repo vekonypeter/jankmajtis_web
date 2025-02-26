b2181ab162108ca1dd81ec236071b14b791b3f80
vekonypeter <vekonypeter94@gmail.com>
2025-02-26 20:33:55 +0100

    Egyéb ellenőrzések, vizsgálatok gazdálkodási menü

---
+++ gazd_egyeb.php
@@ -0,0 +1,112 @@
+<?php
+include('header.php');
+?>
+<div id="content">
+
+    <div id="sideleft">
+
+        <div id="sideBarGrey">
+
+            <div id="headerCaption" class="sideBarElement">
+                Jánkmajtis<br /><span class="lower">község<br />honlapja</span>
+            </div>
+
+            <div id="pageName" class="sideBarElement">
+                Egyéb ellenőrzések, vizsgálatok
+            </div>
+
+        </div>
+    </div>
+
+
+    <div id="sideright">
+    
+        <div class="contentContainer">
+
+            <div class="row new">
+                <div class="newTitle">Dokumentumok</div>
+
+                <div class="col-12">
+                    <table width="100%" border="0" style="margin-left: unset">
+                        <tr>
+                            <td>2025. éves Belső ell.-i Terv Jánkmajtis Község Roma Nemzetiségi Önkormányzata</td>
+                            <td><a href="./docs/gazdalkodas/2025. éves Belső ell.-i Terv Jánkmajtis Község Roma Nemzetiségi Önkormányzata.doc" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>2025. éves Belső ell.-i Terv Jánkmajtis-Csegöld Víziközmű Beruházási Társulás</td>
+                            <td><a href="./docs/gazdalkodas/2025. éves Belső ell.-i Terv Jánkmajtis-Csegöld Víziközmű Beruházási Társulás.doc" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>2025. éves Belső ell.-i Terv Jánkmajtis-Darnó Intézményfenntartó Társulás</td>
+                            <td><a href="./docs/gazdalkodas/2025. éves Belső ell.-i Terv Jánkmajtis-Darnó Intézményfenntartó Társulás.doc" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>2025. évi Kozkázatelemzés Jánkmajtis Község Roma Nemzetiségi Önkormányzata</td>
+                            <td><a href="./docs/gazdalkodas/2025. évi Kozkázatelemzés Jánkmajtis Község Roma Nemzetiségi Önkormányzata.doc" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>2025. évi Kozkázatelemzés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás</td>
+                            <td><a href="./docs/gazdalkodas/2025. évi Kozkázatelemzés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás.doc" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>2025. évi Kozkázatelemzés Jánkmajtis-Darnó Intézményfenntartó Társulás</td>
+                            <td><a href="./docs/gazdalkodas/2025. évi Kozkázatelemzés Jánkmajtis-Darnó Intézményfenntartó Társulás.doc" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Ellenőrzési jelentés utóellenőrzés Jánkmajtis Község Roma Nemzetiségi Önkormányzata</td>
+                            <td><a href="./docs/gazdalkodas/Ellenőrzési jelentés utóellenőrzés Jánkmajtis Község Roma Nemzetiségi Önkormányzata.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Ellenőrzési jelentés utóellenőrzés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás</td>
+                            <td><a href="./docs/gazdalkodas/Ellenőrzési jelentés utóellenőrzés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Ellenőrzési jelentés utóellenőrzés Jánkmajtis-Darnó Intézményfenntartó Társulás</td>
+                            <td><a href="./docs/gazdalkodas/Ellenőrzési jelentés utóellenőrzés Jánkmajtis-Darnó Intézményfenntartó Társulás.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Értesítő levél főkönyvi könyvelés Jánkmajtis Község Roma Nemzetiségi Önkormányzata</td>
+                            <td><a href="./docs/gazdalkodas/Értesítő levél főkönyvi könyvelés Jánkmajtis Község Roma Nemzetiségi Önkormányzata.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Értesítő levél főkönyvi könyvelés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás</td>
+                            <td><a href="./docs/gazdalkodas/Értesítő levél főkönyvi könyvelés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Értesítő levél főkönyvi könyvelés Jánkmajtis-Darnó Intézményfenntartó Társulás</td>
+                            <td><a href="./docs/gazdalkodas/Értesítő levél főkönyvi könyvelés Jánkmajtis-Darnó Intézményfenntartó Társulás.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Értesítő levél utóellenőrzés Jánkmajtis Község Roma Nemzetiségi Önkormányzata</td>
+                            <td><a href="./docs/gazdalkodas/Értesítő levél utóellenőrzés Jánkmajtis Község Roma Nemzetiségi Önkormányzata.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Értesítő levél utóellenőrzés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás</td>
+                            <td><a href="./docs/gazdalkodas/Értesítő levél utóellenőrzés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Értesítő levél utóellenőrzés Jánkmajtis-Darnó Intézményfenntartó Társulás</td>
+                            <td><a href="./docs/gazdalkodas/Értesítő levél utóellenőrzés Jánkmajtis-Darnó Intézményfenntartó Társulás.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Főkönyvi könyvelés ellenőrzési jelentés Jánkmajtis Község Roma Nemzetiségi Önkormányzata</td>
+                            <td><a href="./docs/gazdalkodas/Főkönyvi könyvelés ellenőrzési jelentés Jánkmajtis Község Roma Nemzetiségi Önkormányzata.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Főkönyvi könyvelés ellenőrzési jelentés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás</td>
+                            <td><a href="./docs/gazdalkodas/Főkönyvi könyvelés ellenőrzési jelentés Jánkmajtis-Csegöld Víziközmű Beruházási Társulás.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                        <tr>
+                            <td>Főkönyvi könyvelés ellenőrzési jelentés Jánkmajtis-Darnó Intézményfenntartó Társulás</td>
+                            <td><a href="./docs/gazdalkodas/Főkönyvi könyvelés ellenőrzési jelentés Jánkmajtis-Darnó Intézményfenntartó Társulás.pdf" target="_href">Letöltés<i class="fa-solid fa-menu fa-file-pdf"></i></a></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+include('footer.php');
+?>
